40dd241760034dd38b457f3c961f34b7f16218b3
CHR1S899 <ch.dick@gmx.de>
2018-05-31 23:28:24 +0200

    test
    test

--- appRestApi/main.go
+++ appRestApi/main.go
@@ -43,7 +43,7 @@ func helloApi (w http.ResponseWriter, r *http.Request) {
 
 func main() {
 
-	fmt.Println("test123")
+	fmt.Println("test1234")
 	// Open sql database with credentials from envVar
 	// sql.Open(Driver_name, DB_user + DB_user_password from envVar, DB_adress, DB_name)
 	/*
